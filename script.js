bf38a38ad85bfa3613deed04797f7f0a21c87ca5
unknown <bhanukiran809@gmail.com>
2025-07-09 08:01:47 +0530

    loops

---
+++ script.js
@@ -0,0 +1,13 @@
+let a=2
+do{ document.writeln(a+"<br>" + "i am learning javascript<br>");
+    console.log(a);
+    a++;}
+ while(a<10)
+
+
+    
+for( b=5; b<=20; b++)
+{
+    document.writeln(b + "<br>" + "i am learning for loop <br>");
+    console.log(b);
+}
